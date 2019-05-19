03a2c5d96dab71cff395f51b6a860f690300b9a1
Wouter Nieuwerth <wouternieuwerth@gmail.com>
2019-05-19 13:53:04 +0200

    Update datasciencefunctions.py
    Added remove_outlier function

--- datasciencefunctions.py
+++ datasciencefunctions.py
@@ -161,3 +161,17 @@ def combine_text_columns(data_frame, to_drop=NUMERIC_COLUMNS + LABELS):
     
     # Join all text items in a row that have a space in between
     return text_data.apply(lambda x: " ".join(x), axis=1)
+
+#------------------------------------------------------------------------------
+# accept a dataframe, remove outliers, return cleaned data in a new dataframe
+# see http://www.itl.nist.gov/div898/handbook/prc/section1/prc16.htm
+#------------------------------------------------------------------------------
+def remove_outlier(df_in, col_name):
+    """accept a dataframe, remove outliers, return cleaned data in a new dataframe"""
+    q1 = df_in[col_name].quantile(0.25)
+    q3 = df_in[col_name].quantile(0.75)
+    iqr = q3-q1 #Interquartile range
+    fence_low  = q1-1.5*iqr
+    fence_high = q3+1.5*iqr
+    df_out = df_in.loc[(df_in[col_name] > fence_low) & (df_in[col_name] < fence_high)]
+    return df_out
